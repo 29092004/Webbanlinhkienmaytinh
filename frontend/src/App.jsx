3b79ee85284c09e3464e3d09820605dd66a2e47f
Ngô Văn Viễn <vien11052004@gmail.com>
2026-05-14 17:15:12 +0700

    feat: add otp verification page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Otp from "./pages/Otp";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/otp" element={<Otp />} />
 
       </Routes>
 
